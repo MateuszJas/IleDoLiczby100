4a5177391e72ecc7363b95dfdb305e2bd05eaead
MateuszJas <pphu.jaskolka@hotmail.com>
2022-11-19 14:49:37 +0100

    ok

--- IleDoLiczby/Program.cs
+++ IleDoLiczby/Program.cs
@@ -12,20 +12,20 @@ namespace IleDoLiczby
         {
 
             int a = 1, b = 0, l = 100;
-                                   
 
-            while (a <= l)
+
+            do
             {
-                                              
+
 
                 a = (b + 1) + a;
 
-                
+
                 b++;
 
-            }
+            } while (a <= l);
 
-            Console.WriteLine("Aby uzyskać liczbę " + l + " albo większą należy dodać " + b + " liczb poczynając od 1");
+                Console.WriteLine("Aby uzyskać liczbę " + l + " albo większą należy dodać " + b + " liczb poczynając od 1");
             Console.ReadKey();
 
 
